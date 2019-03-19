4814f08c9bbe0dcf748c45f60df9849a05beeb3c
AliAS105 <42550259+SachinKumar105@users.noreply.github.com>
2019-03-20 01:07:56 +0530

    Issue fixed
    new comment for not attempted questions

--- src/lab/exp6/post_exp_quiz.php
+++ src/lab/exp6/post_exp_quiz.php
@@ -108,11 +108,11 @@ function getOptions(opt){
 		if($_POST['q2'] == "A"){
 			echo "<span style='color:green'>(".$_POST['q2'].") IS CORRECT</span>";
 		}
-		else if($_POST['q1'] == "B" || $_POST['q1'] == "C" || $_POST['q1'] == "D"){
-			echo "<span style='color:red'>(".$_POST['q1'].") IS NOT CORRECT ! CORRECT answer is (A).</span>";
+		else if($_POST['q2'] == "B" || $_POST['q2'] == "C" || $_POST['q2'] == "D"){
+			echo "<span style='color:red'>(".$_POST['q2'].") IS NOT CORRECT ! CORRECT answer is (A).</span>";
 		}
 		else{
-			echo "<span style='color:red'>(".$_POST['q1'].") DIDN'T ATTEMPT.</span>";
+			echo "<span style='color:red'>(".$_POST['q2'].") DIDN'T ATTEMPT.</span>";
 		}
 	}
 	?>
@@ -134,11 +134,11 @@ function getOptions(opt){
 		if($_POST['q3'] == "B"){
 			echo "<span style='color:green'>(".$_POST['q3'].") IS CORRECT</span>";
 		}
-		else if($_POST['q1'] == "A" || $_POST['q1'] == "C" || $_POST['q1'] == "D"){
-			echo "<span style='color:red'>(".$_POST['q1'].") IS NOT CORRECT ! CORRECT answer is (B).</span>";
+		else if($_POST['q3'] == "A" || $_POST['q3'] == "C" || $_POST['q3'] == "D"){
+			echo "<span style='color:red'>(".$_POST['q3'].") IS NOT CORRECT ! CORRECT answer is (B).</span>";
 		}
 		else{
-			echo "<span style='color:red'>(".$_POST['q1'].") DIDN'T ATTEMPT.</span>";
+			echo "<span style='color:red'>(".$_POST['q3'].") DIDN'T ATTEMPT.</span>";
 		}
 	}
 	?>
@@ -160,11 +160,11 @@ function getOptions(opt){
 		if($_POST['q4'] == "A"){
 			echo "<span style='color:green'>(".$_POST['q4'].") IS CORRECT</span>";
 		}
-		else if($_POST['q1'] == "B" || $_POST['q1'] == "C" || $_POST['q1'] == "D"){
-			echo "<span style='color:red'>(".$_POST['q1'].") IS NOT CORRECT ! CORRECT answer is (A).</span>";
+		else if($_POST['q4'] == "B" || $_POST['q4'] == "C" || $_POST['q4'] == "D"){
+			echo "<span style='color:red'>(".$_POST['q4'].") IS NOT CORRECT ! CORRECT answer is (A).</span>";
 		}
 		else{
-			echo "<span style='color:red'>(".$_POST['q1'].") DIDN'T ATTEMPT.</span>";
+			echo "<span style='color:red'>(".$_POST['q4'].") DIDN'T ATTEMPT.</span>";
 		}
 	}
 	?>
@@ -186,11 +186,11 @@ function getOptions(opt){
 		if($_POST['q5'] == "B"){
 			echo "<span style='color:green'>(".$_POST['q5'].") IS CORRECT</span>";
 		}
-		else if($_POST['q1'] == "A" || $_POST['q1'] == "C" || $_POST['q1'] == "D"){
-			echo "<span style='color:red'>(".$_POST['q1'].") IS NOT CORRECT ! CORRECT answer is (B).</span>";
+		else if($_POST['q5'] == "A" || $_POST['q5'] == "C" || $_POST['q5'] == "D"){
+			echo "<span style='color:red'>(".$_POST['q5'].") IS NOT CORRECT ! CORRECT answer is (B).</span>";
 		}
 		else{
-			echo "<span style='color:red'>(".$_POST['q1'].") DIDN'T ATTEMPT.</span>";
+			echo "<span style='color:red'>(".$_POST['q5'].") DIDN'T ATTEMPT.</span>";
 		}
 	}
 	?>
@@ -212,11 +212,11 @@ function getOptions(opt){
 		if($_POST['q6'] == "B"){
 			echo "<span style='color:green'>(".$_POST['q6'].") IS CORRECT</span>";
 		}
-		else if($_POST['q1'] == "A" || $_POST['q1'] == "C" || $_POST['q1'] == "D"){
-			echo "<span style='color:red'>(".$_POST['q1'].") IS NOT CORRECT ! CORRECT answer is (B).</span>";
+		else if($_POST['q6'] == "A" || $_POST['q6'] == "C" || $_POST['q6'] == "D"){
+			echo "<span style='color:red'>(".$_POST['q6'].") IS NOT CORRECT ! CORRECT answer is (B).</span>";
 		}
 		else{
-			echo "<span style='color:red'>(".$_POST['q1'].") DIDN'T ATTEMPT.</span>";
+			echo "<span style='color:red'>(".$_POST['q6'].") DIDN'T ATTEMPT.</span>";
 		}
 	}
 	?>
