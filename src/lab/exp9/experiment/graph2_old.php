3bdcaee4b514b0639b1bbbe65f29b67e839ad457
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2020-03-25 19:25:51 +0530

    un commented feedback links

--- src/lab/exp9/experiment/graph2_old.php
+++ src/lab/exp9/experiment/graph2_old.php
@@ -5,6 +5,8 @@
 ?>
 <html>
  <head>
+<script class='gtm'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W59SWTR');</script>
+
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="flot/excanvas.min.js"></script><![endif]-->
     <script language="javascript" type="text/javascript" src="flot/jquery.js"></script>
